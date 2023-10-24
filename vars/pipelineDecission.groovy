34a3849e125feae6d147c6b160f292ce5b6fea37
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-10-24 19:35:19 -0400

    used the map keyword in in called function

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -3,13 +3,13 @@ def call(Map configMap){
    def application_type = configMap.get("application_type")
     def component = configMap.get("component")
     switch(application_type){
-    case 'nodeJSVMCI':
+        case 'nodeJSVMCI':
                     nodeJSVMCI(configMap) //if the function name is call() in nodeJSVMCI, it is automatically called
                     break
-    case 'javaVM':
+        case 'javaVM':
                     javaVM(configMap)
                     break
-    default :
+        default :
                 echo "Error: enter the correct application type"
                 break
     }
